6e5075b23ad44f0d7b521ca27ca08a322c26d19e
Alexandr <wao564712@gmail.com>
2022-04-10 15:51:07 +0800

    Update wao.php

--- wao.php
+++ wao.php
@@ -1,9 +1,4 @@
-<html lang="en">
-<head>
-<meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>Welcome to wao</title>
-</head>
+<html>
 <body>
 <h1>Hello wao!</h1>
 </body>
